3c770ef5f3d208025b333911dfee155e18cbbb2b
Niels Robin-Aubertin <git@niels.fr>
2023-07-27 11:49:38 -0400

    Support us layouts, .gitconfig dark theme

--- .bash_profile
+++ .bash_profile
@@ -5,6 +5,7 @@ then
   . "${HOME}/.config/pathrc"
 
   if command -v sway >/dev/null; then
+    export XDG_CURRENT_DESKTOP=sway
     exec sway
   fi
 fi
--- .config/sway/scripts/switch_theme.sh
+++ .config/sway/scripts/switch_theme.sh
@@ -11,11 +11,13 @@ set_neovim_bg() {
 if grep gruvbox_light ~/.config/sway/config >/dev/null 2>/dev/null; then
   sed -i -e 's|gruvbox_light|gruvbox_dark|g' ~/.config/sway/config
   sed -i -e 's|gruvbox_light|gruvbox_dark|g' ~/.config/kitty/kitty.conf
+  sed -i -e 's|gruvbox-light|gruvbox-dark|g' ~/.gitconfig
   sed -i -e 's|vim.o.background = "light"|vim.o.background = "dark"|g' ~/.config/nvim/init.vim
   set_neovim_bg dark
 else
   sed -i -e 's|gruvbox_dark|gruvbox_light|g' ~/.config/sway/config
   sed -i -e 's|gruvbox_dark|gruvbox_light|g' ~/.config/kitty/kitty.conf
+  sed -i -e 's|gruvbox-dark|gruvbox-light|g' ~/.gitconfig
   sed -i -e 's|vim.o.background = "dark"|vim.o.background = "light"|g' ~/.config/nvim/init.vim
   set_neovim_bg light
 fi
