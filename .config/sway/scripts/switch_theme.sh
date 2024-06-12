04e081af730c83ab19eac0ff02926db4ce546cf4
Niels Robin-Aubertin <git@niels.fr>
2024-06-12 08:27:38 -0400

    Switch sway keyboard layout to us

--- .config/sway/scripts/switch_theme.sh
+++ .config/sway/scripts/switch_theme.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-set_neovim_bg() {
+set_neovim_theme() {
   for server in $(nvr --nostart --serverlist)
   do
     nvr --nostart --servername "$server" -cc "set background=$1"
@@ -11,15 +11,19 @@ set_neovim_bg() {
 if grep gruvbox_light ~/.config/sway/config >/dev/null 2>/dev/null; then
   sed -i -e 's|gruvbox_light|gruvbox_dark|g' ~/.config/sway/config
   sed -i -e 's|gruvbox_light|gruvbox_dark|g' ~/.config/kitty/kitty.conf
+  sed -i -e 's|gruvbox-light|gruvbox-dark|g' ~/.config/alacritty/alacritty.toml
+  sed -i -e 's|theme "gruvbox-light"|theme "gruvbox-dark"|g' ~/.config/zellij/config.kdl
   sed -i -e 's|gruvbox-light|gruvbox-dark|g' ~/.gitconfig
   sed -i -e 's|vim.o.background = "light"|vim.o.background = "dark"|g' ~/.config/nvim/init.vim
-  set_neovim_bg dark
+  set_neovim_theme dark
 else
   sed -i -e 's|gruvbox_dark|gruvbox_light|g' ~/.config/sway/config
   sed -i -e 's|gruvbox_dark|gruvbox_light|g' ~/.config/kitty/kitty.conf
+  sed -i -e 's|gruvbox-dark|gruvbox-light|g' ~/.config/alacritty/alacritty.toml
+  sed -i -e 's|theme "gruvbox-dark"|theme "gruvbox-light"|g' ~/.config/zellij/config.kdl
   sed -i -e 's|gruvbox-dark|gruvbox-light|g' ~/.gitconfig
   sed -i -e 's|vim.o.background = "dark"|vim.o.background = "light"|g' ~/.config/nvim/init.vim
-  set_neovim_bg light
+  set_neovim_theme light
 fi
 
 pkill --signal SIGUSR1 kitty
